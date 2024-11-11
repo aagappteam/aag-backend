56e21e123654dc3b938b29558cc2c5808a4058ea
Anilkant8576 <anil.mishra@avisoft.io>
2024-11-11 16:16:08 +0530

    inital setup

--- src/main/java/aagapp_backend/services/ResponseService.java
+++ src/main/java/aagapp_backend/services/ResponseService.java
@@ -1,7 +1,7 @@
 package aagapp_backend.services;
 
-import com.community.api.entity.ErrorResponse;
-import com.community.api.entity.SuccessResponse;
+import aagapp_backend.entity.ErrorResponse;
+import aagapp_backend.entity.SuccessResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
--- src/main/java/aagapp_backend/services/exception/CustomErrorController.java
+++ src/main/java/aagapp_backend/services/exception/CustomErrorController.java
@@ -1,16 +1,15 @@
 package aagapp_backend.services.exception;
 
 
-import com.community.api.services.ResponseService;
+import aagapp_backend.services.ResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class CustomErrorController implements ErrorController {
