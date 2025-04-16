4a8841e9a98766382071c89b3634ddba0ebadbd6
Anil Kant Mishra <anilkantmishra356@gmail.com>
2025-04-16 16:02:30 +0530

    worked on fcm token

--- src/main/java/aagapp_backend/services/league/LeagueService.java
+++ src/main/java/aagapp_backend/services/league/LeagueService.java
@@ -38,7 +38,6 @@ import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import javax.naming.LimitExceededException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
