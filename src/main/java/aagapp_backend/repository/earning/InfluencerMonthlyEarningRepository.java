a64c721c7bfb105bbf7437fab305becdd972e1b8
junedbinidreesh <juned.idreesh@celestialitverse.com>
2025-06-26 17:55:06 +0530

    download particAdd advanced filters and CSV export functionality for Influencer Monthly Earnings APIular vendor journey amount

--- src/main/java/aagapp_backend/repository/earning/InfluencerMonthlyEarningRepository.java
+++ src/main/java/aagapp_backend/repository/earning/InfluencerMonthlyEarningRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface InfluencerMonthlyEarningRepository extends JpaRepository<InfluencerMonthlyEarning, Long> {
+public interface InfluencerMonthlyEarningRepository extends JpaRepository<InfluencerMonthlyEarning, Long>, JpaSpecificationExecutor<InfluencerMonthlyEarning> {
     @Query(value = """
     SELECT e FROM InfluencerMonthlyEarning e 
     WHERE e.influencerId = :influencerId 
