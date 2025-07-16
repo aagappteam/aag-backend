25dc52d9b5ddd4f3648f790e7d6d7d247828d87a
junedbinidreesh <juned.idreesh@celestialitverse.com>
2025-07-16 17:16:57 +0530

    payment integration for dummy

--- src/main/java/aagapp_backend/controller/wallet/WalletController.java
+++ src/main/java/aagapp_backend/controller/wallet/WalletController.java
@@ -296,8 +296,14 @@ public class WalletController {
             String uniqueTxnId = "AAG" + System.currentTimeMillis();
 
             // 🔐 Secure API call — will throw BusinessException on failure
-            KwickPayResponse kpResp = walletService.callPayoutGateway(dto,uniqueTxnId , customer);
+//            KwickPayResponse kpResp = walletService.callPayoutGateway(dto,uniqueTxnId , customer);
 
+            // 🔧 Use dummy response for testing
+            KwickPayResponse kpResp = new KwickPayResponse();
+            kpResp.setStatus("TXN");
+            kpResp.setMessage("Transaction successful");
+            kpResp.setTxnid(uniqueTxnId);
+            kpResp.setUtr("UTR1234567890");
 
 
             // 🧩 Handle based on gateway response
