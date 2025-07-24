5a577b06e66317cd6e8268562ea3c22a490df761
Anil Kant Mishra <anilkantmishra356@gmail.com>
2025-07-24 15:38:04 +0530

    Worked on requts

--- src/main/java/aagapp_backend/services/CommonService.java
+++ src/main/java/aagapp_backend/services/CommonService.java
@@ -15,12 +15,14 @@ import aagapp_backend.entity.payment.PaymentEntity;
 import aagapp_backend.entity.players.Player;
 import aagapp_backend.entity.tournament.Tournament;
 import aagapp_backend.entity.wallet.Wallet;
+import aagapp_backend.enums.ActivityType;
 import aagapp_backend.enums.LeagueStatus;
 import aagapp_backend.enums.PaymentStatus;
 import aagapp_backend.enums.TournamentStatus;
 import aagapp_backend.repository.NotificationRepository;
 import aagapp_backend.repository.NotificationShareRepository;
 import aagapp_backend.repository.admin.AdminLogsInterface;
+import aagapp_backend.repository.customcustomer.CustomCustomerRepository;
 import aagapp_backend.repository.earning.InfluencerMonthlyEarningRepository;
 import aagapp_backend.repository.game.PlayerRepository;
 import aagapp_backend.repository.league.LeagueRepository;
@@ -28,8 +30,10 @@ import aagapp_backend.repository.payment.PaymentRepository;
 import aagapp_backend.repository.tournament.TournamentRepository;
 import aagapp_backend.services.exception.BusinessException;
 import aagapp_backend.services.payment.PaymentService;
+import com.amazonaws.services.kms.model.NotFoundException;
 import jakarta.mail.MessagingException;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,6 +55,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import static aagapp_backend.enums.ActivityType.GAME;
+
 @Service
 public class CommonService {
 
@@ -69,6 +75,9 @@ public class CommonService {
 
     @Autowired
     private NotificationService notificationService;
+
+    @Autowired
+    private CustomCustomerRepository customCustomerRepository;
 
     private CustomCustomerService customCustomerService;
     private PlayerRepository playerRepository;
@@ -378,6 +387,7 @@ public void autoRejectUnapprovedTournamentsAndLeagues() throws IOException {
         }
     }
 
+
     @Async
     public void notifyAdminsByRole(int role, String type,String name,Long targetid,String message) throws MessagingException, IOException {
         List<CustomAdmin> admins = entityManager.createQuery(
@@ -405,6 +415,49 @@ public void autoRejectUnapprovedTournamentsAndLeagues() throws IOException {
                 emailService.sendAdminCommonmail(admin, type, name, message);
             }
         }
+    }
+
+
+
+
+    public void addXpPoints(ActivityType activityType, Player player) {
+
+        CustomCustomer customer = playerRepository.findById(player.getPlayerId()).map(Player::getCustomer).orElse(null);
+
+        int xpToAdd = 0;
+
+        switch (activityType) {
+            case GAME:
+                xpToAdd = customer.getIsWeeklyBoosterActive() ? 4 : 2;
+                break;
+            case LEAGUE:
+                xpToAdd = customer.getIsWeeklyBoosterActive() ? 10 : 5;
+                break;
+            case TOURNAMENT:
+                xpToAdd = customer.getIsWeeklyBoosterActive() ? 10 : 5;
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported activity type: " + activityType);
+        }
+
+        customer.setXpPoints(customer.getXpPoints() + xpToAdd);
+        customCustomerRepository.save(customer);
+    }
+
+
+
+    // Every Monday at 00:00 AM
+    @Scheduled(cron = "0 0 0 * * MON")
+    public void resetWeeklyBoosters() {
+        List<CustomCustomer> customers = customCustomerRepository.findAll();
+
+        for (CustomCustomer customer : customers) {
+            customer.setWeeklyBoostersLeft(2);
+            customer.setBoosterActivatedAt(null);
+        }
+
+        customCustomerRepository.saveAll(customers);
+
     }
 
 
