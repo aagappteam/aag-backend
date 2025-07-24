ef820f367dfe7fe096ddb8b545d70223e5286df2
Mo Juned <juned.idreesh@celestialitverse.com>
2025-07-24 17:13:25 +0530

    resolved

--- src/main/java/aagapp_backend/services/CommonService.java
+++ src/main/java/aagapp_backend/services/CommonService.java
@@ -428,6 +428,7 @@ public void autoRejectUnapprovedTournamentsAndLeagues() throws IOException {
 
     }
 
+<<<<<<< HEAD
     @Async
     public void notifyAdminsByRole(int role, String type,String name,Long targetid,String message) throws MessagingException, IOException {
         List<CustomAdmin> admins = entityManager.createQuery(
@@ -456,5 +457,7 @@ public void autoRejectUnapprovedTournamentsAndLeagues() throws IOException {
             }
         }
     }
+=======
+>>>>>>> 51135b0 (resolved conflicts)
 
 }
