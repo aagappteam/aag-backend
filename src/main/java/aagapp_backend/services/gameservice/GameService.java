35841eeff7abd146c3bdda637d57b6ea1100cd37
Anil Kant Mishra <anilkantmishra356@gmail.com>
2025-07-25 16:17:30 +0530

    Work on issue

--- src/main/java/aagapp_backend/services/gameservice/GameService.java
+++ src/main/java/aagapp_backend/services/gameservice/GameService.java
@@ -360,7 +360,7 @@ public class GameService {
         String shareableLink = generateShareableLink(savedGame.getId(),vendorId);
         savedGame.setShareableLink(shareableLink);
 
-        if(gameRequest.getScheduledAt() != null) {
+        if(gameRequest.getScheduledAt() == null) {
             CompletableFuture.runAsync(() -> {
                 followerNotificationService.notifyFollowersInParallel("game", savedGame.getName(), vendorEntity);
             });
